e148b252bdb73e10051b43482d89919d61af8b40
navjot kaur <kaurnavjot1983@outlook.com>
2021-11-23 12:29:51 -0800

    INFRA:Intilaze Console App

---
+++ PersonConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) MumsWhoCode. Learn, Share and Grow. 
+// ---------------------------------------------------------------
+
+using System;
+
+namespace PersonConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
