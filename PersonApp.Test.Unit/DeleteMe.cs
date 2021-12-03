82c2e6f38df9c080e89ab980f0ee220eb47adb4a
navjot kaur <kaurnavjot1983@outlook.com>
2021-12-02 16:04:57 -0800

    INFRA:Intilaze Unit Test

---
+++ PersonApp.Test.Unit/DeleteMe.cs
@@ -0,0 +1,17 @@
+// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Xunit;
+
+namespace PersonApp.Test.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
