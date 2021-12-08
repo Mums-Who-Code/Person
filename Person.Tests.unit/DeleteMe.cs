7e9000a79a99ab2d06429f929885540966c5d1ff
navjot kaur <kaurnavjot1983@outlook.com>
2021-12-07 20:55:48 -0800

    INFRA: Initialize unite Test

---
+++ Person.Tests.unit/DeleteMe.cs
@@ -0,0 +1,16 @@
+// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Xunit;
+
+namespace Person.Tests.unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+
+            Assert.True(true);
+    }
+}
