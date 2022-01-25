3114cae071d085e3f184dbc8a82e2d3fd3a4dfa8
navjot kaur <kaurnavjot1983@outlook.com>
2022-01-25 13:50:43 -0800

    Clean The Code

--- PersonApp.ConsoleApp/Brokers/Loggings/LoggingBroker.cs
+++ PersonApp.ConsoleApp/Brokers/Loggings/LoggingBroker.cs
@@ -10,11 +10,10 @@ namespace PersonApp.ConsoleApp.Brokers.Loggings
     internal class LoggingBroker : ILoggingBroker
     {
         public ILogger logger { get; set; }
-        public LoggingBroker(ILogger<LoggingBroker>logger)=>
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
             this.logger = logger;
 
         public void LogError(Exception exception) =>
-         this.logger.LogError(exception,exception.Message);
-        
+         this.logger.LogError(exception, exception.Message);
     }
 }
