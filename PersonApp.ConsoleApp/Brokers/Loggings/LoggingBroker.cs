82b9881245d9824384fd5d1f220d485d55360686
navjot kaur <kaurnavjot1983@outlook.com>
2022-01-26 10:27:39 -0800

    CODE RUB: Code Cleanup & Alignment

--- PersonApp.ConsoleApp/Brokers/Loggings/LoggingBroker.cs
+++ PersonApp.ConsoleApp/Brokers/Loggings/LoggingBroker.cs
@@ -10,6 +10,7 @@ namespace PersonApp.ConsoleApp.Brokers.Loggings
     internal class LoggingBroker : ILoggingBroker
     {
         public ILogger logger { get; set; }
+
         public LoggingBroker(ILogger<LoggingBroker> logger) =>
             this.logger = logger;
 
