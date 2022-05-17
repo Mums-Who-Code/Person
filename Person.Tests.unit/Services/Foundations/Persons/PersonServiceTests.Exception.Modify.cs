e140f3296f50ec1e24f6b022a9a76c8900381b06
navjot kaur <kaurnavjot1983@outlook.com>
2022-05-17 10:46:05 -0700

    ShouldThrowDependencyValidationExceptionOnModifyIfValidationErrorOccursAndLogIt() -> Pass

---
+++ Person.Tests.unit/Services/Foundations/Persons/PersonServiceTests.Exception.Modify.cs
@@ -0,0 +1,53 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Moq;
+using PersonApp.ConsoleApp.Models.Persons;
+using PersonApp.ConsoleApp.Models.Persons.Exceptions;
+using System;
+using Xunit;
+
+namespace PersonApp.Tests.unit.Services.Foundations.Persons
+{
+    public partial class PersonServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyValidationExceptionOnModifyIfValidationErrorOccursAndLogIt()
+        {
+            //given
+            Person somePerson = CreateRandomPerson();
+            var argumentNullException = new ArgumentNullException();
+
+            var nullArgumentPersonException =
+                new NullArgumentPersonException(argumentNullException);
+
+
+            var expectedPersonDependencyValidationException =
+                new PersonDependencyValidationException(nullArgumentPersonException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdatePerson(It.IsAny<Person>())).
+                    Throws(argumentNullException);
+            //when
+            Action modifyPersonException = () => this.personService.ModifyPerson(somePerson);
+
+            //then
+            Assert.Throws<PersonDependencyValidationException>(modifyPersonException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePerson(It.IsAny<Person>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs
+                    (expectedPersonDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+        }
+
+    }
+}
