45da25e93b68342f79dd5009d519ee3463b488f2
navjot kaur <kaurnavjot1983@outlook.com>
2022-05-17 11:26:21 -0700

    ShouldThrowServiceExceptionOnModifyIfServiceExceptionOccursAndLogIt() -> Pass

--- Person.Tests.unit/Services/Foundations/Persons/PersonServiceTests.Exception.Modify.cs
+++ Person.Tests.unit/Services/Foundations/Persons/PersonServiceTests.Exception.Modify.cs
@@ -49,5 +49,42 @@ namespace PersonApp.Tests.unit.Services.Foundations.Persons
 
         }
 
+
+
+        [Fact]
+        public void ShouldThrowServiceExceptionOnModifyIfServiceExceptionOccursAndLogIt()
+        {
+            //given
+            Person somePerson = CreateRandomPerson();
+            var serviceException = new Exception();
+
+            var failedPersonException = 
+                new FailedPersonServiceException(serviceException);
+
+            var expectedPersonServiceException = 
+                new PersonServiceException(failedPersonException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdatePerson(It.IsAny<Person>())).
+                    Throws(serviceException);
+
+            //when
+            Action modifyPersonAction = () => this.personService.ModifyPerson(somePerson);
+
+            //then
+            Assert.Throws<PersonServiceException>(modifyPersonAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePerson(It.IsAny<Person>()),
+                    Times.Once);
+           
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPersonServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
