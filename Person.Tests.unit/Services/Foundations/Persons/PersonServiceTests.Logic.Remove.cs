dfabf741e889549550960f2a7e5be5421519b6be
navjot kaur <kaurnavjot1983@outlook.com>
2022-05-19 20:24:19 -0700

    public void ShouldRemovePerson -> Fail

---
+++ Person.Tests.unit/Services/Foundations/Persons/PersonServiceTests.Logic.Remove.cs
@@ -0,0 +1,46 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using PersonApp.ConsoleApp.Models.Persons;
+using Xunit;
+
+namespace PersonApp.Tests.unit.Services.Foundations.Persons
+{
+    public partial class PersonServiceTests
+    {
+        [Fact]
+        public void ShouldRemovePerson()
+        {
+            //given
+            Person randomPerson = CreateRandomPerson();
+            Person inputPerson = randomPerson;
+            Person deletedPerson = inputPerson;
+            Person expectedPerson = deletedPerson;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeletePerson(inputPerson)).
+                    Returns(deletedPerson);
+
+            //when
+            Person actualPerson = this.personService.RemovePerson(inputPerson);
+
+            //then
+            actualPerson.Should().BeEquivalentTo(expectedPerson);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.DeletePerson(inputPerson),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
